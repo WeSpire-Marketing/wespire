d15ed0c557834e0c0455be485cd565664b8b8aa7
s9379992g <s9379992g@gmail.com>
2022-11-29 18:39:17 +0200

    removed h1, h5, h6, headings from article and story text editor

--- studio/schemas/documents/stories.js
+++ studio/schemas/documents/stories.js
@@ -99,7 +99,13 @@ export default {
           type: 'array',
           of: [
             {
-              type: 'block'
+              type: 'block',
+              styles: [
+                { title: 'Normal', value: 'normal' },
+                { title: 'H2', value: 'h2' },
+                { title: 'H3', value: 'h3' },
+                { title: 'H4', value: 'h4' }
+              ]
             },
             {
               type: 'videoId',
