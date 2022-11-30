9e5648fe8c591939bf0c572a1a6c7db5cdfaf474
s9379992g <s9379992g@gmail.com>
2022-11-30 09:29:55 +0200

    added default quote to the article and story schema

--- studio/schemas/documents/article.js
+++ studio/schemas/documents/article.js
@@ -108,10 +108,11 @@ export default {
         {
           type: 'block',
           styles: [
-            { title: 'Normal', value: 'normal' },
             { title: 'H2', value: 'h2' },
             { title: 'H3', value: 'h3' },
-            { title: 'H4', value: 'h4' }
+            { title: 'H4', value: 'h4' },
+            { title: 'Quote', value: 'blockquote' },
+            { title: 'Normal', value: 'normal' }
           ],
           marks: {
             annotations: [
--- studio/schemas/documents/stories.js
+++ studio/schemas/documents/stories.js
@@ -101,10 +101,11 @@ export default {
             {
               type: 'block',
               styles: [
-                { title: 'Normal', value: 'normal' },
                 { title: 'H2', value: 'h2' },
                 { title: 'H3', value: 'h3' },
-                { title: 'H4', value: 'h4' }
+                { title: 'H4', value: 'h4' },
+                { title: 'Quote', value: 'blockquote' },
+                { title: 'Normal', value: 'normal' }
               ]
             },
             {
