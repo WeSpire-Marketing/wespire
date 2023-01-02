936afc8312216de02acd0e28501ebe89e4a3f301
s9379992g <s9379992g@gmail.com>
2023-01-02 16:32:11 +0200

    .

--- web/components/sections/HomeTemplate/index.jsx
+++ web/components/sections/HomeTemplate/index.jsx
@@ -2,8 +2,8 @@ import RenderSections from '../../RenderSections'
 
 const HomeTemplate = ({sections}) => {
   const sectionsArr = []
-  // sectionsArr.push(sections.heroSection)
-  // sectionsArr.push(sections.longSection)
+  sectionsArr.push(sections.heroSection)
+  sectionsArr.push(sections.longSection)
   sectionsArr.push(sections.sliderSection)
   sectionsArr.push(sections.cardsSection)
   sectionsArr.push(sections.ctaSection)
