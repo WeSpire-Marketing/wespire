7008bc024a50c9a1335d89789b3a9945735bd66d
IiIymik <sambotemp@gmail.com>
2023-07-07 17:57:25 +0300

    add useInview for LongSection

--- web/components/sections/LongSection/index.jsx
+++ web/components/sections/LongSection/index.jsx
@@ -1,13 +1,16 @@
 import Link from 'next/link'
-import {motion} from 'framer-motion'
+import {motion, useInView} from 'framer-motion'
 
 import Row from '../../Row'
 import ImageCard from '../../ImageCard'
 import Icon from '../../icons/AnimatedStroke'
 
 import {injectIconToSpanStr} from '../../../utils'
+import {useRef} from 'react'
 
 export default function LongSection({title = '', rows}) {
+  const ref = useRef(null)
+  const inView = useInView(ref, {once: true})
   return (
     <section
       className="longsection relative overflow-hidden bg-gallery pt-[70px] pb-[100px]
@@ -31,60 +34,64 @@ export default function LongSection({title = '', rows}) {
           ))}
         </h2>
 
-        <div className="flex flex-col gap-[72px] sm:gap-[81px] md:gap-[90px] lg:gap-[99px]">
-          {rows.map(({_key, title, text, imageData, link, cardColor}, idx) => {
-            return (
-              <Row
-                className={`${
-                  idx % 2 === 0 ? 'flex-col md:flex-row' : 'flex-col md:flex-row-reverse'
-                } flex items-center gap-6`}
-                key={_key}
-              >
-                <div className="longsection__col flex w-full">
-                  <ImageCard image={imageData} color={cardColor} />
-                </div>
-
-                <div className="longsection__col w-full px-2 sm:px-0">
-                  <div className="md:mx-auto md:max-w-[286px] lg:max-w-[416px]">
-                    <motion.h3
-                      className="longsection__heading heading-3 mb-4"
-                      initial={{opacity: 0, x: idx % 2 == 0 ? 100 : -100}}
-                      whileInView={{opacity: 1, x: 0}}
-                      transition={{duration: 1}}
-                      viewport={{once: true}}
-                    >
-                      {title}
-                    </motion.h3>
-
-                    <motion.p
-                      className="longsection__text body-m mb-6 text-secondary"
-                      initial={{opacity: 0, x: idx % 2 == 0 ? 100 : -100}}
-                      whileInView={{opacity: 1, x: 0}}
-                      transition={{duration: 1, delay: 0.2}}
-                      viewport={{once: true}}
-                    >
-                      {text}
-                    </motion.p>
-
-                    {link.internal ? (
-                      <Link href={link.url} passHref>
-                        <a className="longsection__link stroke-btn">{link.text}</a>
-                      </Link>
-                    ) : (
-                      <a
-                        className="longsection__link stroke-btn"
-                        href={link.url}
-                        target="_blank"
-                        rel="noreferrer noopener"
-                      >
-                        {link.text}
-                      </a>
-                    )}
+        <div
+          ref={ref}
+          className="flex flex-col gap-[72px] sm:gap-[81px] md:gap-[90px] lg:gap-[99px]"
+        >
+          {inView &&
+            rows.map(({_key, title, text, imageData, link, cardColor}, idx) => {
+              return (
+                <Row
+                  className={`${
+                    idx % 2 === 0 ? 'flex-col md:flex-row' : 'flex-col md:flex-row-reverse'
+                  } flex items-center gap-6`}
+                  key={_key}
+                >
+                  <div className="longsection__col flex w-full">
+                    <ImageCard image={imageData} color={cardColor} />
                   </div>
-                </div>
-              </Row>
-            )
-          })}
+
+                  <div className="longsection__col w-full px-2 sm:px-0">
+                    <div className="md:mx-auto md:max-w-[286px] lg:max-w-[416px]">
+                      <motion.h3
+                        className="longsection__heading heading-3 mb-4"
+                        initial={{opacity: 0, x: idx % 2 == 0 ? 100 : -100}}
+                        whileInView={{opacity: 1, x: 0}}
+                        transition={{duration: 1}}
+                        viewport={{once: true}}
+                      >
+                        {title}
+                      </motion.h3>
+
+                      <motion.p
+                        className="longsection__text body-m mb-6 text-secondary"
+                        initial={{opacity: 0, x: idx % 2 == 0 ? 100 : -100}}
+                        whileInView={{opacity: 1, x: 0}}
+                        transition={{duration: 1, delay: 0.2}}
+                        viewport={{once: true}}
+                      >
+                        {text}
+                      </motion.p>
+
+                      {link.internal ? (
+                        <Link href={link.url} passHref>
+                          <a className="longsection__link stroke-btn">{link.text}</a>
+                        </Link>
+                      ) : (
+                        <a
+                          className="longsection__link stroke-btn"
+                          href={link.url}
+                          target="_blank"
+                          rel="noreferrer noopener"
+                        >
+                          {link.text}
+                        </a>
+                      )}
+                    </div>
+                  </div>
+                </Row>
+              )
+            })}
         </div>
       </div>
     </section>
