9cff218919fe1223cd8eb444813e73337a10b16d
IiIymik <sambotemp@gmail.com>
2023-07-18 11:47:12 +0300

    add google tag manager

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -8,8 +8,6 @@ import '../styles/custom-properties.css'
 
 const MyApp = ({Component, pageProps}) => {
   const tag = pageProps?.config?.googleAnalyticsTag ?? ''
-  const loadGTA = Boolean(pageProps?.content?.sections[0]?._type === 'landingTemplateCompetitor')
-
   return (
     <>
       <Script
@@ -18,14 +16,13 @@ const MyApp = ({Component, pageProps}) => {
         async
       />
       {/* <!-- Google Tag Manager --> */}
-      {!loadGTA && (
-        <Script strategy="worker" async>
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${tag}');`}
-        </Script>
-      )}
+      <Script async src={`https://www.googletagmanager.com/gtag/js?id=${tag}`} />
+      <Script id="google-analytics">
+        {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', ${tag});`}
+      </Script>
       {/* <!-- End Google Tag Manager --> */}
       <Suspense>
         <AnimatePresence mode="wait">
